64a8b03d77c2188bb920aa06b5373e4f57fb6239
s4ysjc8vg7-source <s4ysjc8vg7@privaterelay.appleid.com>
2026-09-19 00:04:18 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE='myplanner-v15-layout';
-const ASSETS=['./','./index.html','./styles.css','./app.js?v=15','./manifest.webmanifest'];
+const CACHE='myplanner-v18-consolidated';
+const ASSETS=['./','./index.html','./styles.css','./app.js?v=18','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('message',e=>{if(e.data==='SKIP_WAITING')self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
